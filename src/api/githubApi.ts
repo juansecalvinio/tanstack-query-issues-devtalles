a7589d3e9f3c62065b6eb1e2aba9aeaf13557b9d
Juanse Calviño <j.s.calvinio@gmail.com>
2024-04-30 23:46:34 -0300

    Add environments variables to GitHub Access Token

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const access_token =
-  "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";
+  process.env.GITHUB_ACCESS_TOKEN;
 
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
