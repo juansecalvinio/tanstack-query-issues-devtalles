d78e29f093a66c846f44714301334d69212def63
Juanse Calviño <j.s.calvinio@gmail.com>
2024-04-30 23:48:35 -0300

    Add env file

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const access_token =
-  process.env.GITHUB_ACCESS_TOKEN;
+const access_token = process.env.GITHUB_ACCESS_TOKEN;
 
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
